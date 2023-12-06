929ab6c34bef5131b59fb4979ebe0049e064d040
omarIsmail7980 <omar.business7980@gmail.com>
2023-12-05 23:13:27 -0600

    added model call

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const PORT = 8080;
+const textToImage = require("./routes/txtToImg");
 
 const app = express();
 app.use(cors());
+app.use("/toimg", textToImage);
 
 app.listen(PORT,()=>{
     console.log(`Listening on PORT: ${PORT}`);
--- routes/txtToImg.js
+++ routes/txtToImg.js
@@ -1,11 +1,33 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
+const { DiffusionPipeline } = require("@huggingface/inference");
+
+const modelId = "stabilityai/stable-diffusion-2";
+const hfAccessToken = "<YOUR_ACCESS_TOKEN>";
 
 
-router.post(()=>{
-    try {
-        
-    } catch (error) {
-        console.log(error);
-    }
-})
+
+
+router.post("/", async (req, res) => {
+  try {
+    const model = await DiffusionPipeline.from_pretrained(modelId, {
+        accessToken: hfAccessToken,
+      });
+
+      const inputs = "your input text here";
+      const parameters = { negative_prompt: "your negative prompt here" };
+
+      const result = await model.textToImage({ inputs, parameters });
+
+        console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
+
+
+
+
+
