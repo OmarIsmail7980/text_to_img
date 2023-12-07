2d7fe29034e548e33873e209ed6451ae5815df61
omarIsmail7980 <omar.business7980@gmail.com>
2023-12-06 23:00:08 -0600

    adding logic

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const PORT = 8080;
 const textToImage = require("./routes/txtToImg");
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 app.use("/toimg", textToImage);
+
+app.get("/", (req,res)=>{
+    res.send("server is up and running")
+})
 
 app.listen(PORT,()=>{
     console.log(`Listening on PORT: ${PORT}`);
--- routes/txtToImg.js
+++ routes/txtToImg.js
@@ -3,31 +3,29 @@ const router = express.Router();
 const { DiffusionPipeline } = require("@huggingface/inference");
 
 const modelId = "stabilityai/stable-diffusion-2";
-const hfAccessToken = "<YOUR_ACCESS_TOKEN>";
+const hfAccessToken = "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";
 
-
-
-
+// Endpoint to generate images
 router.post("/", async (req, res) => {
   try {
-    const model = await DiffusionPipeline.from_pretrained(modelId, {
-        accessToken: hfAccessToken,
-      });
+    // Create an instance of the DiffusionPipeline directly
+    const model = new DiffusionPipeline(modelId, { accessToken: hfAccessToken });
 
-      const inputs = "your input text here";
-      const parameters = { negative_prompt: "your negative prompt here" };
+    // Extract input text from the request body
+    const { text } = req.body;
 
-      const result = await model.textToImage({ inputs, parameters });
+    // Generate image based on input text
+    const parameters = { negative_prompt: "Your negative prompt here" };
+    const result = await model.textToImage({ inputs: text, parameters });
 
-        console.log(result);
+    console.log(result);
+    // Send the generated image in the response
+    res.set("Content-Type", "image/png"); // Set the content type as per your generated image type
+    res.send(Buffer.from(result)); // Assuming the result is a Buffer representing the image
   } catch (error) {
-    console.log(error);
+    console.error("Error:", error);
+    res.status(500).json({ error: "Failed to generate image" });
   }
 });
 
 module.exports = router;
-
-
-
-
-
